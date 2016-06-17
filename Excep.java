1ca7c31f8ecccb494595c5150a632ad9bcf7c6e4
Hyxmy <x1821378430@qq.com>
2016-06-17 11:00:40 +0800

    excep

---
+++ Excep.java
@@ -0,0 +1,18 @@
+/**
+ * @(#)Excep.java
+ *
+ *
+ * @author 
+ * @version 1.00 2016/6/7
+ */
+
+class Excep {
+
+    public static void main(String[] args){
+    	String[] str={"45","1th","123","123er45"};
+    	
+    }
+    
+    
+}
+
